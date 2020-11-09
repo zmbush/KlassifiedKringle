e699d8420bc7590736916827f3ba04e2feee327a
Zachary Bush <zach@zmbush.com>
2020-11-08 19:23:14 -0800

    pep8 format

--- kk.py
+++ kk.py
@@ -1,6 +1,7 @@
-#A secret santa assignment bot?
-#I would like it to take data from a google sheet and then send parts of that data to members of the discord server
-#I don't want to know who got me and vice versa
+# A secret santa assignment bot?
+# I would like it to take data from a google sheet and then send parts of that
+#  data to members of the discord server
+# I don't want to know who got me and vice versa
 
 import discord
 import os
@@ -13,9 +14,11 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
@@ -28,6 +31,5 @@ async def on_message(message):
     if message.content.startswith("$assign"):
         member = message.guild.get_member_named(os.getenv('TEST_USER'))
         await message.channel.send(member.mention)
-
 
 client.run(os.getenv('BOT_TOKEN'), bot=True)
